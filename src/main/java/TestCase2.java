a89164fe54bafff7ab10d865e3b8c2ec67cfc9d6
Elaman <kazakbaevelaman066@gmail.com>
2022-05-23 11:38:29 -0500

    third commit

--- src/main/java/TestCase2.java
+++ src/main/java/TestCase2.java
@@ -4,5 +4,7 @@ public class TestCase2 {
     @Test
     public void Test2(){
         System.out.println("Put your code here");
+        System.out.println("another day with git");
+        System.out.println("practice practice practice");
     }
 }
