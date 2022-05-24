04f28af1fa8719d939c2a2d1b5cd8f92a9e6aeab
dsuvorova <daryasuvorova18@gmail.com>
2022-05-23 20:32:41 -0500

    Get etsy with driver

--- src/main/java/TestCase3.java
+++ src/main/java/TestCase3.java
@@ -1,8 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestCase3 {
     @Test
     public void Test3(){
-        System.out.println("Put your code here");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.etsy.com/");
+        driver.close();
     }
 }
