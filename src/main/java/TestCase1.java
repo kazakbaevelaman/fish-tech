d1dc8b89a61f8db4f55303c8bf70248826115f85
gladyslaserbeam <gladysleyzer@gmail.com>
2022-05-23 21:31:09 -0400

    update 1 fathers day button

--- src/main/java/TestCase1.java
+++ src/main/java/TestCase1.java
@@ -1,10 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestCase1 {
     @Test
-    public void Test1(){
-        System.out.println("Put your code here");
-        System.out.println("changes");
-        System.out.println("new line");
+    public void fathersDayButton(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.etsy.com/?ref=lgo");
+
+
+
+
     }
 }
